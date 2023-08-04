c812006b949177c47fcfee40b3a389c4994e2d0d
Almira Khafizova <mimirka80@gmail.com>
2023-08-04 22:57:47 +0200

    Created new file ImagesListCell and constant reuseIdentifier. Included extension to ImagesListViewController. Added image, label, button and all constraints to them.

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Almira Khafizova on 04.08.23.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+
+}
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -7,16 +7,22 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController {
-
+final class ImagesListViewController: UIViewController {
+    
     @IBOutlet private var tableView: UITableView!
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
     }
-
-
+    
 }
 
 extension ImagesListViewController: UITableViewDataSource {
@@ -26,9 +32,23 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     //метод протокола должен возвращать ячейку. У класса UITableViewCell есть дефолтный конструктор, который не принимает никаких аргументов.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        //Метод, который из всех ячеек, зарегистрированных в таблице, возвращает ячейку по заранее добавленному идентификатору:
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        //Чтобы работать с ячейкой как с экземпляром класса ImagesListCell, нам надо провести приведение типов:
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+            //при неуспешной кастомизации ячейки к нужному типу приложение можно аварийно завершить или прологгировать ошибку создания
+        }
+        
+        configCell(for: imageListCell, with: indexPath) // 3
+        return imageListCell // 4
     }
     
+}
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) { }
 }
 
 
@@ -36,4 +56,6 @@ extension ImagesListViewController: UITableViewDelegate {
     //Этот метод отвечает за действия, которые будут выполнены при тапе по ячейке таблицы. «Адрес» ячейки, который содержится в indexPath, передаётся в качестве аргумента.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
 }
+
+
 
