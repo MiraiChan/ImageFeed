19ce801f2d5f3a29284cb3ec25fe9150183412a9
Almira Khafizova <mimirka80@gmail.com>
2023-08-15 23:03:00 +0200

    SingleImageViewController's added and constraints  were set.

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Almira Khafizova on 15.08.23.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+}
