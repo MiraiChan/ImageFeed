d278094b7bfbf9604758e4d7d0c09e105de1d6aa
Almira Khafizova <mimirka80@gmail.com>
2023-08-25 09:58:58 +0200

    New file "Constants" with content inside is created.

---
+++ ImageFeed/ImagesList/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Almira Khafizova on 25.08.23.
+//
+
+import Foundation
+
+let accessKey = "<xHdiZjeVewTI-J6qKZGZFImsyvTndYog9M94OBkOELQ>"
+let secretKey = "<OxUia9bRGuv01C_Al6uKbgNCrnshrQP5TjmRMbW5Al8>"
+let redirectURI = "<вurn:ietf:wg:oauth:2.0:oob>"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!
