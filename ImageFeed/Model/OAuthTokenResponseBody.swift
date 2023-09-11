017cd1d643b7c2afcbd70846389cafd53b3072a5
Almira Khafizova <mimirka80@gmail.com>
2023-09-11 13:33:51 +0200

    Deleted comments.

--- ImageFeed/Model/OAuthTokenResponseBody.swift
+++ ImageFeed/Model/OAuthTokenResponseBody.swift
@@ -14,7 +14,7 @@ struct OAuthTokenResponseBody: Decodable {
     let scope: String
     let createdAt: Int
     
-    // соотносим ответ с dконстантами
+    // соотносим ответ с константами
     enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
         case tokenType = "token_type"
