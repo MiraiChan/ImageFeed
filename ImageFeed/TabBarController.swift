86fe5b2bfebbab886b2ff7b25d0c3e8baf1dadd9
Almira Khafizova <mimirka80@gmail.com>
2023-09-20 21:49:02 +0300

    TabBarController was implemented using method awakeFromNib. ProfileViewController was created using code and deleted in IB.

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -7,22 +7,26 @@
 
 import Foundation
 import UIKit
- 
-final class TabBarController: UITabBarController {
-        override func awakeFromNib() {
-            super.awakeFromNib()
-            let storyboard = UIStoryboard(name: "Main", bundle: .main)
-                
-            let imagesListViewController = storyboard.instantiateViewController(
-                withIdentifier: "ImagesListViewController"
-            )
-                
-            let profileViewController = storyboard.instantiateViewController(
-                withIdentifier: "ProfileViewController"
-            )
-               
-           self.viewControllers = [imagesListViewController, profileViewController]
-           }
-           
 
+final class TabBarController: UITabBarController {
+override func awakeFromNib() {
+    super.awakeFromNib()
+    let storyboard = UIStoryboard(name: "Main", bundle: .main)
+    
+    let imagesListViewController = storyboard.instantiateViewController(
+        withIdentifier: "ImagesListViewController"
+    )
+        
+    let profileViewController = ProfileViewController()
+    
+    profileViewController.tabBarItem = UITabBarItem(
+    title: nil,
+    image: UIImage(named: "tab_profile_active"),
+    selectedImage: nil
+)
+        
+    self.viewControllers = [imagesListViewController, profileViewController]
 }
+}
+
+
